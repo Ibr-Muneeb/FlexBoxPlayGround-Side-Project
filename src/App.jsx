f73e8881ab96eb12b5079cb847233bd73032ed60
Ibr-Muneeb <ibrahimmuneeb22@gmail.com>
2026-01-15 22:18:15 -0500

    Initial Vite + React setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Flexbox Playground</h1>
+      </header>
+
+      <main className="main">
+        <aside className="sidebar">
+          Controls
+        </aside>
+
+        <section className="preview">
+          Preview
+        </section>
+
+        <aside className="css-output">
+          CSS Output
+        </aside>
+      </main>
+    </div>
+  );
+}
+
+export default App;
